fc9e2ceada76dc92e1121e32162db463bc57bf7f
Taurus Omejia <bolenton@gmail.com>
2017-08-28 14:29:12 -0500

    directives excercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses: Course[];
+
+  num: number = 4;
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'Course-'+ this.num });
+    this.num++
+  }
+
+  onRemove(course: Course){
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1)
+    this.num --;
+  }
+
+  onChange(course: Course){
+    course.name = "Updated Course"+ course.id;  
+  }
+
+  loadCourses(){ 
+    this.courses = [
+      {id: 1, name: "Course-1"},
+      {id: 1, name: "Course-2"},
+      {id: 1, name: "Course-3"}
+    ];
+  }
+
+  trackCourse(index, course: Course) {
+    return course ? course.id : undefined;
+  }
+}
+
+class Course {
+  id: number;
+  name: string;
 }
