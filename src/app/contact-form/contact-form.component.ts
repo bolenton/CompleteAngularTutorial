1ced8f123203087a26f293f4e5acbc30e1705b7b
Taurus Omejia <bolenton@gmail.com>
2017-08-30 17:24:19 -0500

    - added forms and validations

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent{
+  contactMethods: Array<Object> = [
+    {id:1, name: 'home'},
+    {id:2, name: 'work'}
+  ]
 
-  constructor() { }
 
-  ngOnInit() {
+  log(x){
+    console.log(x);
+  }
+
+  submit(f){
+    console.log(f.value)
+    console.log(f);
   }
 }
